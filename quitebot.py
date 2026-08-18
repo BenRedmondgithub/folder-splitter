6e4580dab79aa876eae7d3f43a11ef64d99a6c64
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2026-08-18 18:06:25 +0100

    Update quitebot.py

--- quitebot.py
+++ quitebot.py
@@ -1,13 +1,13 @@
-user_input = input("Press Enter to continue...")
+import random
+
+myQuote = ["Fuck me dude", "Thanks to God, Jesus, and the bear conductor.", "dickballs", "Fuck yeah dude", "CLUNKLES, USE YOUR FISTS.", "Am I fucking over", "what a mark", "This shit pulls no punches at all.", "This has one job and it is to ruin my day"]
+randomQuote = random.choice(myQuote)
+
 while True:
+
+    user_input = input("Press Enter to continue...")
     if user_input == "!quote":
-        print (randomQuote)
-    else:
-        print ("try again!")
-
-
-
-        myQuote = ["Fuck me dude", "Thanks to God, Jesus, and the bear conductor.", "dickballs", "Fuck yeah dude", "CLUNKLES, USE YOUR FISTS.", "Am I fucking over", "what a mark", "This shit pulls no punches at all.", "This has one job and it is to ruin my day"]
-        import random
         randomQuote = random.choice(myQuote)
-        print (randomQuote)
+        print(randomQuote)
+    else:
+        print("try again!")
